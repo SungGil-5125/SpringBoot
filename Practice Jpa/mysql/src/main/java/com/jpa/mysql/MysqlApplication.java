61648bdeaadfafb3869b87cbdadc86b42c3ad758
SungGil-5125 <sunggil0125@naver.com>
2022-04-11 23:03:39 +0900

    Setting mysql

---
+++ Practice Jpa/mysql/src/main/java/com/jpa/mysql/MysqlApplication.java
@@ -0,0 +1,13 @@
+package com.jpa.mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlApplication.class, args);
+	}
+
+}
---
+++ Practice Jpa/mysql/src/test/java/com/jpa/mysql/MysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpa.mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
