55f74d00652fadb46b32e95daa2a59e0242b4398
SungGil-5125 <sunggil0125@naver.com>
2022-04-12 22:36:32 +0900

    JpaRepository Test

--- Practice Jpa/mysql/src/test/java/com/jpa/mysql/Repository/UserRepositoryTest.java
+++ Practice Jpa/mysql/src/test/java/com/jpa/mysql/Repository/UserRepositoryTest.java
@@ -1,4 +1,38 @@
+package com.jpa.mysql.Repository;
+
+import com.jpa.mysql.Entity.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+//@SpringBootTest
+@Transactional
+@SpringBootTest
 class UserRepositoryTest {
-  
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void create() {
+        //dto써서 toEntity로 받기
+        User user = User.builder()
+                        .acount("Test User 03")
+                        .email("sunggil0125@naver.com")
+                        .phoneNumber("010-6546-6622")
+                        .createAt(LocalDateTime.now())
+                        .createdBy("admin")
+                        .build();
+
+        User newuser = userRepository.save(user);
+
+    }
 }
