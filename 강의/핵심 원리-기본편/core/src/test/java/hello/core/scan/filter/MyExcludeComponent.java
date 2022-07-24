4812fdd9f08e04d17c174e31828e402dc0757d09
SungGil-5125 <sunggil0125@naver.com>
2022-07-24 20:41:17 +0900

    ✨ :: Component 직접 생성

---
+++ 강의/핵심 원리-기본편/core/src/test/java/hello/core/scan/filter/MyExcludeComponent.java
@@ -0,0 +1,11 @@
+package hello.core.scan.filter;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MyExcludeComponent {
+
+
+}
