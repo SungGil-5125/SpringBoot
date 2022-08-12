454d960a23514d2fa7c7a2fc2e736b42f47fb3d6
SungGil-5125 <sunggil0125@naver.com>
2022-08-12 21:55:53 +0900

    request bean scope

---
+++ 강의/핵심 원리-기본편/core/src/main/java/hello/core/common/MyLogger.java
@@ -0,0 +1,39 @@
+package hello.core.common;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.UUID;
+
+@Component
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class MyLogger {
+
+    private String uuid;
+    private String requestURL;
+
+    public void setRequestURL(String requestURL) {
+        this.requestURL = requestURL;
+    }
+
+    public void log(String message) {
+        System.out.println("[" + uuid + "]" + "[" + requestURL + "]" + message);
+    }
+
+    // 요청이 들어올때
+    @PostConstruct
+    public void init() {
+        uuid = UUID.randomUUID().toString();
+        System.out.println("[" + uuid + "] request scope bean create : " + this);
+    }
+
+    // 요청이 끝나면
+
+    @PreDestroy
+    public void close() {
+        System.out.println("[" + uuid + "] request scope bean close : " + this);
+    }
+}
