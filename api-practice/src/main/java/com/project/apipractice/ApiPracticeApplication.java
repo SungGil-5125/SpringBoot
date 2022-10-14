53107c25b4e2fdb7e600313c5031a202ee766e8f
김성길 <82089918+SungGil-5125@users.noreply.github.com>
2022-10-14 15:55:20 +0900

    🎉 :: project setting

---
+++ api-practice/src/main/java/com/project/apipractice/ApiPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.project.apipractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPracticeApplication.class, args);
+	}
+
+}
---
+++ api-practice/src/test/java/com/project/apipractice/ApiPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.apipractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
